abf34502525360a029ae15dd5b23e54e6942f0cf
Ricky <rickyliew@hotmail.com>
2020-08-12 16:43:27 +1000

    Add proptypes for COngrats component

--- jotto/redux/src/Congrats.test.js
+++ jotto/redux/src/Congrats.test.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+// import checkPropTypes from "check-prop-types";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
-import { enhanceUnexpectedTokenMessage } from "@jest/transform/build/enhanceUnexpectedTokenMessage";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  *
@@ -16,11 +18,14 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  // const setupProps={...defaultProps}
+  // return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -35,4 +40,18 @@ test("renders non-empty congrats msg when 'success' prop is TRUE", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
+  // #region
+  // const propError = checkPropTypes(
+  //   Congrats.propTypes,
+  //   expectedProps,
+  //   "prop",
+  //   Congrats.name
+  // );
+  // expect(propError).toBeUndefined();
+  // #endregion
 });
