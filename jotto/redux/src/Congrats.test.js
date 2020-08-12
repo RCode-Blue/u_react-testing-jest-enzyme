d4645a60bb05867efb8d84b1b8d4ebb91eaf5e0f
Ricky <rickyliew@hotmail.com>
2020-08-12 15:57:18 +1000

    congrats

---
+++ jotto/redux/src/Congrats.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import { findByTestAttr } from "../test/testUtils";
+import Congrats from "./Congrats";
+import { enhanceUnexpectedTokenMessage } from "@jest/transform/build/enhanceUnexpectedTokenMessage";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ *
+ * @param {Factory function to create a ShallowWrapper for the COngrats component.
+ * @function setup
+ * @param{object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1);
+});
+
+test("renders no text when 'success' prop is FALSE", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
+
+test("renders non-empty congrats msg when 'success' prop is TRUE", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
