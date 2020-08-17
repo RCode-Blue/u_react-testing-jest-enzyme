f194c70acdc712d1de6c1b748b8524fb8addcc14
Ricky <rickyliew@hotmail.com>
2020-08-18 09:24:50 +1000

    Install Redux Thunk middleware

--- jotto/redux/test/testUtils.js
+++ jotto/redux/test/testUtils.js
@@ -1,16 +1,21 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../src/reducers";
+import { middlewares } from "../src/configureStore";
 
 /**
  * Create a testing store with imnported reducers, middleware and initial state.
+ * globals: rootReducer, middleWare
  * @param {object} initialState - Initialstate for store.
  * @function storeFactory
  * @returns{Store} - Redux store
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
