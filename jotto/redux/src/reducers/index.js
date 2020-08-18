af66d809fdcc78bcf593f86a865e00dd0479a108
Ricky <rickyliew@hotmail.com>
2020-08-18 10:46:41 +1000

    Set up guessWords test

--- jotto/redux/src/reducers/index.js
+++ jotto/redux/src/reducers/index.js
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux';
-import success from './successReducer'
+import { combineReducers } from "redux";
+import success from "./successReducer";
+import guessedWords from "./guessedWordsREducer";
 
 export default combineReducers({
   success,
+  guessedWords,
 });
